cf5fd1d8a634a3f2ada558f9a8f50aca3197115b
Leonardo Almeida Cunha <lalmcunha@gmail.com>
2022-09-23 12:16:37 -0300

    Outro teste

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import { server } from "./http";
 import './websocket/ChatService'
 
-// Testando git tag
+// Testando git tag 2
 
 server.listen(3000, () => 
   console.log(`rocketsocket v.${process.env.npm_package_version} running on port 3000`)
